5c9eeb9bc006b760bf5da49ebc6a010d983c0712
Shaik Ikbhal Basha <shaikh.bashas@jabong.com>
2021-10-20 06:09:44 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+func printArr(arr []int){
+	for i:=0;i<len(arr);i++ {
+		fmt.Printf("%d ", arr[i]);
+	}
+}
+func main() {
+	arr := []int{10, 7, 9, 2, 8,
+                            3, 5, 4, 6, 1}
+	printArr(arr);
+	for i:=0; i< len(arr);i++ {
+		arr[i] = i+1
+	}
+  fmt.Println("\nafter sorting")
+	printArr(arr);
+	// fmt.Println("Hello, playground")
+	
+
+}
